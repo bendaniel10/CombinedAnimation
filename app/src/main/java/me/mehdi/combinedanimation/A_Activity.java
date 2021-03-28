dc85497ad8bd2d9df1c281f87b0d05683c60c617
danielben <bendaniel20@gmail.com>
2021-03-28 18:58:20 +0200

    add animation listener and override activity pending transactions

--- app/src/main/java/me/mehdi/combinedanimation/A_Activity.java
+++ app/src/main/java/me/mehdi/combinedanimation/A_Activity.java
@@ -23,6 +23,23 @@ public class A_Activity extends AppCompatActivity {
             @Override
             public void onClick(View button) {
                 Animation viewAnimation = AnimationUtils.loadAnimation(A_Activity.this, R.anim.animate_view);
+                viewAnimation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        startActivity(new Intent(A_Activity.this, B_Activity.class));
+                        overridePendingTransition(R.anim.activity_enter, R.anim.activity_exit);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
                 button.startAnimation(viewAnimation);
             }
         });
